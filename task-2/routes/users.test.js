69456ab5c07b020f2992314bbaf355f0a006cf45
kalhollywood <kal.hollywood@gmail.com>
2022-06-02 12:09:50 +0100

    Task 2.4 complete

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -5,6 +5,9 @@ import app from "../app.js";
 import request from "supertest";
 import assert, { doesNotMatch } from "assert";
 import { Router } from "express";
+import { getUsers, getUserById } from "../models/users.js";
+
+// TASK 2.3
 
 describe("GET /users", function () {
   test("responds with json", async function getUsers() {
@@ -24,6 +27,29 @@ describe("GET /users", function () {
           username: expect.any(String),
         }),
       ])
+    );
+    console.log(response.body.payload);
+  });
+});
+
+// TASK 2.4
+
+describe("GET /users/4", function () {
+  test("responds with 4th user", async function getUserById() {
+    const response = await request(app).get("/users/4");
+    expect(response.statusCode).toBe(200);
+    console.log(response.statusCode);
+    expect(response.body).toMatchObject({
+      success: true,
+      payload: expect.any(Object),
+    });
+    console.log(response.body);
+
+    expect(response.body.payload).toEqual(
+      expect.objectContaining({
+        id: 4,
+        username: expect.any(String),
+      })
     );
     console.log(response.body.payload);
   });
