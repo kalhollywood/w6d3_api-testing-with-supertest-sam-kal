1df3dd7ae9b72339663f93cf8b8481c7e746d821
kalhollywood <kal.hollywood@gmail.com>
2022-06-02 11:52:42 +0100

    Hopefully forked/committed to my repo

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -1,1 +1,30 @@
 // Write your tests for task 2 in this file. Plan out what you need to import/require.
+// Write your tests for task 2 in this file. Plan out what you need to import/require.
+import { test, expect, describe } from "@jest/globals";
+import app from "../app.js";
+import request from "supertest";
+import assert, { doesNotMatch } from "assert";
+import { Router } from "express";
+
+describe("GET /users", function () {
+  test("responds with json", async function getUsers() {
+    const response = await request(app).get("/users");
+    expect(response.statusCode).toBe(200);
+    console.log(response.statusCode);
+    expect(response.body).toMatchObject({
+      success: true,
+      payload: expect.any(Array),
+    });
+    console.log(response.body);
+
+    expect(response.body.payload).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          username: expect.any(String),
+        }),
+      ])
+    );
+    console.log(response.body.payload);
+  });
+});
